221daae70cc9782e4403c8b7fe18f262161b9d12
hxxdll <hxxdll00@gmail.com>
2025-07-16 11:09:59 +0900

    회원가입
    회원가입

--- src/App.js
+++ src/App.js
@@ -4,25 +4,33 @@ import './styles/css/sb-admin-2.min.css';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from './pages/Home';
 import CourseList from './pages/course/List';
-
+import Join from "./pages/Join"
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="card shadow mb-4">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/course/list" element={<CourseList />} />
-              </Routes>
+      <Routes>
+        <Route path="/join" element={<Join />} />
+        <Route path="/join/signup/student" element={<Join />} />
+        <Route path="/join/signup/teacher" element={<Join />} />
+        
+        <Route path="/*" element={
+          <Layout>
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="card shadow mb-4">
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/course/list" element={<CourseList />} />
+                  </Routes>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </Layout>
+          </Layout>
+        } />
+      </Routes>
     </BrowserRouter>
   );
 }
 
-export default App; // 이 줄이 있는지 확인!
+export default App;
